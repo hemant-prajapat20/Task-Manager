1aaa72d9d0741015da89ae7819f683bbc9de573d
hemant-prajapat20 <prajapathemant564@gmail.com>
2026-02-24 22:51:07 +0530

    create task model

---
+++ Backend/models/task_model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+        title:{
+           type:String, 
+           required:true 
+        },
+        description:{
+            type:String,
+        },
+
+        priority:{
+            type:String,
+            enum:["low","medium","high"],
+            default:"low",
+        },
+        status:{
+            type:String,
+            enum:["pending","in-progress","completed"],
+            default:"pending",
+        },          
+      dueDate:{
+        type:Date,
+      },
+
+        assignedTo:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        },  
+    ],
+    createdBy:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+      },
+    ],
+    attachements:[{
+        type:String,
+    }],
+
+    todoChecklist:[todoSchema],
+    
+    progress:{
+        type:Number,
+        default:0,
+    },
+},
+    {
+       timestamps:true,
+    }
+)
+const Task= mongoose.model("Task", taskSchema);
+
+export default Task;
+
+
